09ba28257dc6e9b61103a203676dd07e663aebf5
doomangang <sp0943@cau.ac.kr>
2024-11-21 22:38:20 +0900

    impl:ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,3 +1,16 @@
-//
-// Created by Jihyun Jeong on 11/21/24.
-//
+#include <iostream>
+
+int main (void)
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << "memory address of string variable: " << &str << std::endl;
+	std::cout << "memory address held by 'stringPTR': " << stringPTR << std::endl;
+	std::cout << "memory address held by 'stringREF': " << &stringREF << std::endl;
+	std::cout << std::endl;
+	std::cout << "value of string variable: " << str << std::endl;
+	std::cout << "value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "value pointed to by stringREF: " << stringREF << std::endl;
+}
